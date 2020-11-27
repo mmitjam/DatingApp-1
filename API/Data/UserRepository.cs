d4c4fa6730c0bc63331af2309b00ff33b3f3de55
Dave Canatsey <dcanatsey@gmail.com>
2020-11-27 14:58:53 -0500

    finally fixed photoUrl

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -22,12 +22,16 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUsernameAsync(string userName)
         {
-            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == userName);
+            return await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == userName);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.ToListAsync<AppUser>();
+            return await _context.Users
+                .Include(p => p.Photos)
+                .ToListAsync<AppUser>();
         }
 
         public async Task<bool> SaveAllAsync()
